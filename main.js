fedaf251a3a1d020d40b5eef169389369d20ea90
OchoaUnicen <ignacio.ochoa.unicen@gmail.com>
2024-02-23 02:36:36 -0300

    initial implementation

---
+++ main.js
@@ -0,0 +1,49 @@
+
+
+$(() => {
+  
+    $('#guest, .close-modal').on('click', () => {
+      $('.login-modal, .overlay').fadeOut();
+    });
+    
+    
+    $('#login').on('click', () => {
+      $('.modal-content').slideToggle();
+      $('.username').focus();
+    });
+    
+    $('.register-link').on('click', () => {
+      $('.register-slide').addClass('active-register');
+      $('.login-form').addClass('move-form');
+    });
+    
+    $('.close-register').on('click', () => {
+      $('.register-slide').removeClass('active-register');
+      $('.login-form').removeClass('move-form');
+      $('.username').focus();
+    });
+    
+  });
+
+
+
+  let ab = document.getElementById("access");
+  let wrong_pass = document.getElementById("wrong_pass");
+  ab.addEventListener("click", key);
+
+  function key (){
+    let pass = document.getElementById("pass").value;
+    if (pass === "\x53\x6b\x34\x38\x37\x39\x36\x32\x31\x51\x66\x40\x65\x6c\x52\x36\x76\x4e"){
+        let del = document.getElementById("del"); 
+        del.style.display = "";
+        wrong_pass.innerText = "Joining";
+
+        $('.login-modal, .overlay').fadeOut();
+        document.body.style.background = "none";
+        onContentLoad();
+    }
+    else{        
+        wrong_pass.innerText = "Wrong Key";
+    }
+  }
+
